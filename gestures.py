8647db05e45493f559f64f8495cb100f3c3b5339
Rob <rob.sutton123@gmail.com>
2021-03-22 13:49:58 -0400

    created all classes for each gesture and used the new Player class instead of gesture class to be inherited

--- gestures.py
+++ gestures.py
@@ -1,18 +1,34 @@
 
 
-class Gestures:
+class Player:
     def __init__(self):
-        self.gesture = []
-        self.player_gesture = ''
+        self.player_gesture = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
-class Rock(Gestures):
-    def rock(self):
+class Rock(Player):
+    def __init__(self):
         self.rock_beats = ['Scissors', 'Lizard']
         super().__init__()
 
-class Paper(Gestures):
-    def paper(self):
-        self.scissor_beats = ['Rock', 'Spock']
+
+class Paper(Player):
+    def __init__(self):
+        self.paper_beats = ['Rock', 'Spock']
         super().__init__()
 
-class
+
+class Scissors(Player):
+    def __init__(self):
+        self.scissor_beats = ['Paper', 'Lizard']
+        super().__init__()
+
+
+class Lizard(Player):
+    def __init__(self):
+        self.lizard_beats = ['Spock', 'Paper']
+        super().__init__()
+
+
+class Spock(Player):
+    def __init__(self):
+        self.spock_beats = ['Scissors', 'Rock']
+        super().__init__()
