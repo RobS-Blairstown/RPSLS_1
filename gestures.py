54122dc2e44a1d8d263ac21145de60ca43217561
Rob <rob.sutton123@gmail.com>
2021-03-23 10:51:23 -0400

    Created a display gesture class

--- gestures.py
+++ gestures.py
@@ -1,38 +1,46 @@
 
 
 class Player:
-    def __init__(self):
-        self.player_gesture = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-
-class Rock(Player):
-    def __init__(self):
-        self.rock_beats = ['Scissors', 'Lizard']
-        super().__init__()
+    def __init__(self, gesture):
+        self.gesture = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
 
-class Paper(Player):
-    def __init__(self):
-        self.paper_beats = ['Rock', 'Spock']
-        super().__init__()
-
-
-class Scissors(Player):
-    def __init__(self):
-        self.scissor_beats = ['Paper', 'Lizard']
-        super().__init__()
-
-
-class Lizard(Player):
-    def __init__(self):
-        self.lizard_beats = ['Spock', 'Paper']
-        super().__init__()
-
-
-class Spock(Player):
-    def __init__(self):
-        self.spock_beats = ['Scissors', 'Rock']
-        super().__init__()
-
+# class Rock(Player):
+#     def __init__(self):
+#         self.rock_beats = ['Scissors', 'Lizard']
+#         super().__init__()
+#
+#
+# class Paper(Player):
+#     def __init__(self):
+#         self.paper_beats = ['Rock', 'Spock']
+#         super().__init__()
+#
+#
+# class Scissors(Player):
+#     def __init__(self):
+#         self.scissor_beats = ['Paper', 'Lizard']
+#         super().__init__()
+#
+#
+# class Lizard(Player):
+#     def __init__(self):
+#         self.lizard_beats = ['Spock', 'Paper']
+#         super().__init__()
+#
+#
+# class Spock(Player):
+#     def __init__(self):
+#         self.spock_beats = ['Scissors', 'Rock']
+#         super().__init__()
+#
 
 # display gesture options class
+class DisplayGesture(Player):
+    def __init__(self):
+        self.display_gesture = input(f'{Player.gesture}')
+
+
 # assign a gesture to a player class
+
+class 
